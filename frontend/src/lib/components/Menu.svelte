89683aed78001fd7ace75409b26a8e54a78be1dd
Marci0817 <mihmar17@gmail.com>
2025-04-20 11:32:35 +0200

    Default menu, admin

---
+++ frontend/src/lib/components/Menu.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { accessToken } from '$lib/stores/auth';
+
+	function logout() {
+		$accessToken = '';
+		window.location.href = '/login';
+	}
+</script>
+
+<div class="mb-10 flex h-20 w-full items-center justify-between bg-gray-100 px-4 shadow-md">
+	<h1 class="text-2xl font-bold">Díjcsomagok</h1>
+	<p class="text-gray-500">Válassza ki az Önnek legjobban megfelelő díjcsomagot.</p>
+	<button onclick={logout} class="text-blue-400 underline">Kijelentkezés</button>
+</div>
