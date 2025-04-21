0f6ac6038f7f2c660e64121c635ede8c1769ce96
CsupityTamás <160401832+csupitytamas@users.noreply.github.com>
2025-04-21 20:30:58 +0200

    fix t_id

--- backend/app/schemas/tudastar_schema.py
+++ backend/app/schemas/tudastar_schema.py
@@ -1,21 +1,25 @@
 from pydantic import BaseModel
 
 class TudastarBase(BaseModel):
+    t_id: int
     kategoria: int
     kerdes_szoveg: str
     valasz_szoveg: str
 
 class TudastarCreate(TudastarBase):
+    t_id: int
     kategoria: int
     kerdes_szoveg: str
     valasz_szoveg: str
 
 class TudastarUpdate(TudastarBase):
+    t_id: int
     kategoria: int = None
     kerdes_szoveg: str = None
     valasz_szoveg: str = None
 
 class TudastarOut(TudastarBase):
+    t_id: int
     kategoria: int
     kerdes_szoveg: str
     valasz_szoveg: str
